54fe3ef883ccc2a3c985d4b67ac7f495c1fb5c48
calmiAI <xinyu_std@163.com>
2024-01-08 00:22:01 +0800

    Update WebUI

--- ui/app.py
+++ ui/app.py
@@ -9,9 +9,9 @@ from tools.data.file_ops import get_absolute_path
 STABLE_DIFFUSION_MODEL_ROOT = './downloads/stable_diffusion/'
 
 
-def txt2img(model, model_type, prompt, negative_prompt, scheduler,
-            num_inference_steps, batch_size, height, width,
-            random_seed, guidance_scale, clip_skip):
+def run_txt2img(model, model_type, prompt, negative_prompt, scheduler,
+                num_inference_steps, batch_size, height, width,
+                random_seed, guidance_scale, clip_skip, progress=gr.Progress(track_tqdm=True)):
     model = get_absolute_path(relative_path=STABLE_DIFFUSION_MODEL_ROOT + model)
     ret = Text2ImagePipeline(prompt=prompt,
                              negative_prompt=negative_prompt,
@@ -29,10 +29,7 @@ def txt2img(model, model_type, prompt, negative_prompt, scheduler,
                              clip_skip=clip_skip,
                              use_lora=False,
                              requires_safety_checker=False).__call__(show=True)
-    if len(ret) < 2:
-        return ret[0], None
-    else:
-        return ret[0], ret[1]
+    return ret
 
 
 def get_model_filenames(root_dir=STABLE_DIFFUSION_MODEL_ROOT, suffix='safetensors'):
@@ -58,39 +55,36 @@ def main():
             with gr.Column():
                 with gr.Row():
                     model = gr.Dropdown(label='Choose the model', choices=get_model_filenames())
-                    model_type = gr.Dropdown(label='Choose the model type', choices=['Stable Diffusion 1.5', 'Stable Diffusion XL'])
+                    model_type = gr.Dropdown(label='Choose the model type',
+                                             choices=['Stable Diffusion 1.5', 'Stable Diffusion XL'])
                     scheduler = gr.Dropdown(label='Choose the scheduler', choices=get_scheduler_names())
                 prompt = gr.Textbox(label='prompt', interactive=True, show_copy_button=True)
                 negative_prompt = gr.Textbox(label='negative prompt', show_copy_button=True)
                 with gr.Row():
-                    num_inference_steps = gr.Slider(label='num of inference steps', minimum=10, maximum=100, value=20, step=1)
+                    num_inference_steps = gr.Slider(label='num of inference steps', minimum=10, maximum=100, value=20,
+                                                    step=1)
                     batch_size = gr.Slider(label='batch size', minimum=1, maximum=64, value=1, step=1)
                 with gr.Row():
                     height = gr.Number(label='height', value=1024, precision=0, minimum=512, maximum=None, step=1)
                     width = gr.Number(label='width', value=640, precision=0, minimum=512, maximum=None, step=1)
                 with gr.Row():
                     random_seed = gr.Number(label='random seed', value=-1, precision=0, minimum=-1)
-                    guidance_scale = gr.Number(label='guidance scale', value=7.0, minimum=1.0)
-                    clip_skip = gr.Number(label='clip skip', value=2, precision=0)
+                    guidance_scale = gr.Slider(label='guidance scale', value=7.0, minimum=1.0, maximum=20.0)
+                    clip_skip = gr.Slider(label='clip skip', value=2, minimum=1, maximum=15, step=1)
 
                 run_btn = gr.Button(value='Run')
 
             with gr.Column():
-                with gr.Row():
-                    output_1 = gr.Image(type='pil')
-                    output_2 = gr.Image(type='pil')
+                output = gr.Gallery(height='60vh')
 
-        run_btn.click(fn=txt2img,
+        run_btn.click(fn=run_txt2img,
                       inputs=[
                           model, model_type, prompt, negative_prompt, scheduler,
                           num_inference_steps, batch_size, height, width,
                           random_seed, guidance_scale, clip_skip
                       ],
-                      outputs=[
-                          output_1,
-                          output_2
-                      ])
-    ui.launch()
+                      outputs=output)
+    ui.queue().launch()
 
 
 if __name__ == '__main__':
