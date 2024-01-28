a3295aefba63986f95423b4a2dad3a33d4d27721
calmiAI <xinyu_std@163.com>
2024-01-28 17:18:29 +0800

    Add civitai lora

--- src/diffusion/prompt.py
+++ src/diffusion/prompt.py
@@ -46,35 +46,64 @@ def read_prompt_from_str(prompt: str, lpw: bool) -> str:
     return prompt
 
 
+def parse_lora_description(text: str):
+    lora_name, lora_scale = text.split(':')[1:]
+    lora_scale = float(lora_scale)
+    return lora_name, lora_scale
+
+
+def get_item_value(key: str, text: str):
+    start_idx = text.find(key)
+    end_idx = start_idx
+    for i in range(start_idx, len(text)):
+        if text[i] == ',':
+            end_idx = i
+            break
+    value = text[start_idx+len(key)+2:end_idx]
+    return value
+
+
 def read_civitai_generate_data(civitai_generate_file_path):
     generate_data_file_path = get_absolute_path(civitai_generate_file_path)
     generate_data_str = read_txt(generate_data_file_path)
 
     negative_prompt_idx = generate_data_str.find("Negative prompt")
     steps_idx = generate_data_str.find("Steps")
-    eta_idx = generate_data_str.find("Eta")
-    size_idx = generate_data_str.find("Size")
-    seed_idx = generate_data_str.find("Seed")
-    model_idx = generate_data_str.find("Model")
-    sampler_idx = generate_data_str.find("Sampler")
-    cfg_scale_idx = generate_data_str.find("CFG scale")
-    clip_skip_idx = generate_data_str.find("Clip skip")
-    model_hash_idx = generate_data_str.find("Model hash")
-    hires_upscale_idx = generate_data_str.find("Hires upscale")
-    hires_upscaler_idx = generate_data_str.find("Hires upscaler")
-    denoising_strength_idx = generate_data_str.find("Denoising strength")
 
     prompt = generate_data_str[:negative_prompt_idx].rstrip()
+
+    # parse lora from prompt
+    lora = {}
+    matches = re.finditer(pattern=r'<(.*?)>', string=prompt)
+    for match in matches:
+        content = match.group(1)
+        lora_name, lora_scale = parse_lora_description(content)
+        # lora exists
+        lora['enable'] = True
+        lora['civitai'] = True
+        lora['model'] = ["./downloads/lora/"]
+        if 'weights' not in lora:
+            lora['weights'] = [f"{lora_name}.safetensors"]
+        else:
+            lora['weights'].append(f"{lora_name}.safetensors")
+        if 'scales' not in lora:
+            lora['scales'] = [lora_scale]
+        else:
+            lora['scales'].append(lora_scale)
+        lora['location'] = 'unet'
+
+    prompt = remove_a1111_prompt_lora_description(prompt)
+
     negative_prompt = generate_data_str[negative_prompt_idx + 17: steps_idx].rstrip()
-    num_inference_steps = int(generate_data_str[steps_idx + 7:eta_idx - 2])
-    size = generate_data_str[size_idx + 6:seed_idx - 2]
+    num_inference_steps = int(get_item_value("Steps", generate_data_str))
+    size = get_item_value("Size", generate_data_str)
     width, height = list(map(int, size.split('x')))
-    random_seed = int(generate_data_str[seed_idx + 6:model_idx - 2])
-    scheduler = generate_data_str[sampler_idx + 9:cfg_scale_idx - 2]
-    guidance_scale = int(generate_data_str[cfg_scale_idx + 11:clip_skip_idx - 2])
-    clip_skip = int(generate_data_str[clip_skip_idx + 11:model_hash_idx - 2])
-    scale_factor = int(generate_data_str[hires_upscale_idx + 15:hires_upscaler_idx - 2])
-    upscaler = generate_data_str[hires_upscaler_idx + 16:denoising_strength_idx - 2]
+    random_seed = int(get_item_value("Seed", generate_data_str))
+    scheduler = get_item_value("Sampler", generate_data_str)
+    guidance_scale = float(get_item_value("CFG scale", generate_data_str))
+    clip_skip = int(get_item_value("Clip skip", generate_data_str))
+    scale_factor = int(get_item_value("Hires upscale", generate_data_str))
+    upscaler = get_item_value("Hires upscaler", generate_data_str)
 
     generate_data = {
         "prompt": prompt,
@@ -88,6 +117,7 @@ def read_civitai_generate_data(civitai_generate_file_path):
         "clip_skip": clip_skip,
         "scale_factor": scale_factor,
         "upscaler": upscaler,
+        "lora": lora,
     }
 
     print("-------------CIVITAI GENERATE DATA----------------")
@@ -158,7 +188,7 @@ def remove_a1111_prompt_lora_description(prompt: str) -> str:
         else:
             if not stack:
                 result += char
-    result = merge_whitespace(result)
+    result = merge_whitespace(result).strip()
     return result
 
 
