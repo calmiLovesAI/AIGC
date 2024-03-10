05b0f8ba804ede51dcc0f2c01c75a42e85aacc20
calmiAI <xinyu_std@163.com>
2024-03-11 00:03:20 +0800

    Update

---
+++ app.py
@@ -0,0 +1,5 @@
+from scripts.text2image import main as txt2img_main
+
+
+if __name__ == '__main__':
+    txt2img_main()
---
+++ src/pipelines/detection.py
@@ -0,0 +1,9 @@
+class ObjectDetection2dPipeline:
+    def __init__(self):
+        pass
+
+    def from_model(self, model_name, dataset: str = "coco"):
+        pass
+
+    def predict(self, img):
+        pass
