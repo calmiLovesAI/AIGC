c751d463312681ed2141b14ba5827e6227bde916
calmiAI <xinyu_std@163.com>
2024-03-17 17:25:21 +0800

    add yolo_8 urls

---
+++ src/models/urls.py
@@ -0,0 +1,13 @@
+
+# https://github.com/ultralytics/ultralytics
+ULTRALYTICS = {
+    # detection
+    "yolo_8_n": "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8n.pt",
+    "yolo_8_s": "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8s.pt",
+    "yolo_8_m": "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8m.pt",
+    "yolo_8_l": "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8l.pt",
+    "yolo_8_x": "https://github.com/ultralytics/assets/releases/download/v8.1.0/yolov8x.pt",
+
+    # segmentation
+
+}
---
+++ src/models/yolo.py
@@ -0,0 +1,22 @@
+from ultralytics import YOLO
+
+
+def yolo_detect():
+    pass
+
+
+def yolo_segment():
+    pass
+
+
+def yolo_pose():
+    pass
+
+
+def yolo_obb():
+    """
+    Oriented object detection goes a step further than object detection and introduce an extra angle to
+    locate objects more accurate in an image.
+    :return:
+    """
+    pass
