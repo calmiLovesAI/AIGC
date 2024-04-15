dc34b000cfaf87bb62d0b8997f7e312a64be2171
calmiAI <xinyu_std@163.com>
2024-04-15 08:12:42 +0800

    Fix pytorch lightning rank_zero_only import error

---
+++ src/pipelines/diffusion/prompt.py
@@ -0,0 +1,4 @@
+
+
+def long_prompt_weighting():
+    pass
---
+++ src/pipelines/diffusion/txt2img.py
@@ -0,0 +1,17 @@
+import torch
+
+
+class StableDiffusionV15Pipeline:
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
+
+
+class StableDiffusionXLPipeline:
+    def __init__(self):
+        pass
+
+    def __call__(self, *args, **kwargs):
+        pass
